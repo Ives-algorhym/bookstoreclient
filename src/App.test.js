57b74102e1ab9230ba03dc7ce7d0cb576b249b86
Algo-Rhym <algorhym80@gmail.com>
2021-10-26 23:49:27 -0400

    integrate ui

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,9 @@ import {render} from "@testing-library/react";
 import App from './App'
 
 describe('App component', () =>{
-    it('should render App whit error', () =>{
+    
+        it('should render App whit error', () =>{
         const{ asFragment} = render(<App />);
         expect(asFragment()).toMatchSnapshot 
-    });
+        });
 });
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const enviromentUrls = new Map();
+
+enviromentUrls.set('localhost','http://localhost:8080')
+enviromentUrls.set('algo-book-client','http://algo-book-client.herokuapp.com')
+
+export default enviromentUrls.get(window.location.hostname);
