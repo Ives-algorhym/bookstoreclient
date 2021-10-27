fa0114152a48bcd8dd9e301632e35ff4bcd9e177
Algo-Rhym <algorhym80@gmail.com>
2021-10-26 23:44:17 -0400

    integrating ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import axios from 'axios';
+import baseUrl from './config';
+
+
 const App = () =>{
+
+    
+
+    axios(`${baseUrl}/api/v1/books`).then(books =>{
+        console.log(books);
+    })
+    
     return <div>My first component</div>;
 };
 
