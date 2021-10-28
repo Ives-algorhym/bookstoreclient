316888c4fe781d7163243d06dd59e7c312728354
Algo-Rhym <algorhym80@gmail.com>
2021-10-28 17:11:02 -0400

    changes http for https

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const enviromentUrls = new Map();
 
 enviromentUrls.set("localhost","http://localhost:8080");
-enviromentUrls.set("algo-book-client.herokuapp.com","http://algo-book-store.herokuapp.com/");
+enviromentUrls.set("algo-book-client.herokuapp.com","https://algo-book-store.herokuapp.com/");
 
 export default enviromentUrls.get(window.location.hostname);
